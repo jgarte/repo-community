2b38aeb32e31d6e8a4e89664d927dfc51b5a4be2
Kyryl Melekhin <k.melekhin@gmail.com>
2021-08-02 13:13:39 +0000

    [ungoogled]chromium: -> xorg

--- community/chromium/files/chromium.sh
+++ community/chromium/files/chromium.sh
@@ -2,7 +2,5 @@
 
 # shellcheck disable=2086
 exec /usr/lib/chromium/chromium \
-    --ozone-platform=wayland \
-    --enable-features=UseOzonePlatform \
     --enable-gpu-rasterization \
     $CHROME_FLAGS "$@"
